eaa2ba0c7f2be1af7a3322d4ed55646e216effef
MonckingJ <nikhil73533@gmail.com>
2025-03-27 01:41:06 +0530

    Modified requirements.txt

--- frontend/src/components/LiveKitModal.jsx
+++ frontend/src/components/LiveKitModal.jsx
@@ -12,7 +12,7 @@ const LiveKitModal = ({ setShowSupport }) => {
     try {
       console.log("run")
       const response = await fetch(
-        `/api/getToken?name=${encodeURIComponent(userName)}`
+        `https://voice-agent-om22.onrender.com/getToken?name=${encodeURIComponent(userName)}`
       );
       const token = await response.text();
       setToken(token);
